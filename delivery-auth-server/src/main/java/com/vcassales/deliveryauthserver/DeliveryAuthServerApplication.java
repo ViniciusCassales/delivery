758d165d82722e51fb7b260436ebc0d7e620b019
Vinicius Cassales - Level4 <viniciuscassales@hotmail.com>
2019-12-10 18:36:19 -0300

    Alterada versão do springBoot do 2.2.2 para 1.5.4

--- delivery-auth-server/src/main/java/com/vcassales/deliveryauthserver/DeliveryAuthServerApplication.java
+++ delivery-auth-server/src/main/java/com/vcassales/deliveryauthserver/DeliveryAuthServerApplication.java
@@ -2,12 +2,13 @@ package com.vcassales.deliveryauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeliveryAuthServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryAuthServerApplication.class, args);
 	}
-
 }
