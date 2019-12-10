1715d157ac883255523931d4a1e230942073bea5
Vinicius Cassales - Level4 <viniciuscassales@hotmail.com>
2019-12-10 11:15:15 -0300

    Commit inicial do servidor de autenticação

---
+++ delivery-auth-server/src/test/java/com/vcassales/deliveryauthserver/DeliveryAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcassales.deliveryauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
